d7899d665a42affb5a28e4f080389958af5a2ab9
manucourtade <courtademanuel@outlook.es>
2026-09-22 19:09:54 -0300

    ADD: Cross origins activated...

--- src/main/java/com/biblioteca/ejercicios_practica/controller/AuthController.java
+++ src/main/java/com/biblioteca/ejercicios_practica/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "https://library-mngt.vercel.app/")
 public class AuthController {
 
     private final AuthService authService;
