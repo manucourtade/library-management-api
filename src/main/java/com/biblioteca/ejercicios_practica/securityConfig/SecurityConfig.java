06b1127a7b5f417980ff079bcc33e16eee6e417a
manucourtade <courtademanuel@outlook.es>
2026-09-22 19:16:35 -0300

    FIX: CORS CONFIGURATION method fixed

--- src/main/java/com/biblioteca/ejercicios_practica/securityConfig/SecurityConfig.java
+++ src/main/java/com/biblioteca/ejercicios_practica/securityConfig/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "https://library-mngt.vercel.app")); 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
